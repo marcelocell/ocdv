6cd4990d00c18fbba578da5f85c6d51502da9311
Marcelo Junior <marcelo_andreia@hotmail.com>
2025-07-07 20:44:59 -0300

    feat(init): Initial commit
    First commit, blog base and first post.

---
+++ blog/2025/07-09/hello_world.md
@@ -0,0 +1,29 @@
+---
+title: Hello, World!
+authors: marcelojr
+image: hello_there.jpg
+tags: [hello]
+keywords: [blog]
+---
+
+![Obi Wan Kenobi falando "Hello, world!"](img/hello_there.jpg)
+
+Existe uma tradição (ou lenda urbana) entre desenvolvedores: nunca se começa uma nova linguagem, framework ou biblioteca sem antes escrever um bom e velho “hello world”. Não sou exatamente supersticioso… mas não tinha como ser diferente no primeiro post. 😂
+
+{/* truncate */}
+
+Depois de muito tempo pensando, finalmente tirei da gaveta uma ideia antiga: criar um espaço para compartilhar aprendizados, reflexões e boas histórias do mundo da tecnologia e desenvolvimento de software.
+
+Inspirado pelo trabalho do meu colega [Sérgio Lopes](https://www.linkedin.com/in/sergio-lopes-20131a33/) no [Developer Initiative](https://deviniciative.wordpress.com/), decidi apostar no formato escrito e aplicar a famosa [Técnica de Feynman](https://deviniciative.wordpress.com/2024/05/26/tecnica-de-estudo-aplicando-a-tecnica-de-feynman/ "Técnica de Estudo | Aplicando a Técnica de Feynman").
+
+A proposta d'O Código Dá 20 (ou, se preferir, OCDV) é ser direto, técnico e bem-humorado, com um estilo mão na massa, na linha do [Baeldung](https://www.baeldung.com/), salpicado de referências a Star Wars, memes, Dan Brown e com cobertura de piada do pavê. Se você curte desenvolvimento, arquitetura de software, boas práticas e um pouco de cultura dev, fica o convite para acompanhar.
+
+> Este projeto acaba de declarar "Olá" para o mundo!
+
+<div class="text--center">
+  ![Pintinho saindo do ovo](img/egg.jpg)
+</div>
+
+```js
+return 20;
+```
---
+++ docusaurus.config.ts
@@ -0,0 +1,161 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const contextPath: string = '/ocdv/';
+
+const config: Config = {
+  title: 'O Código Dá 20',
+  tagline: 'Reflexões e práticas para quem vive na lógica e sonha em alto nível.',
+  favicon: contextPath + 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://marcelocell.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: contextPath,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'marcelocell', // Usually your GitHub org/user name.
+  projectName: 'ocdv', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: false,
+        pages: false,
+        svgr: false,
+        gtag: false,
+        googleTagManager: false,
+        sitemap: {},
+        blog: {
+          blogTitle: '',
+          blogDescription: 'Reflexões e práticas para quem vive na lógica e sonha em alto nível.',
+          postsPerPage: 20,
+          blogSidebarTitle: 'Últimas postagens',
+          blogSidebarCount: 20,
+          showReadingTime: true,
+          routeBasePath: '/',
+          archiveBasePath: 'arquivo',
+          authorsBasePath: 'autores',
+          blogListComponent: '@theme/BlogListPage',
+          feedOptions: {
+            type: ['rss', 'atom', 'json'],
+            xslt: true,
+          },
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: contextPath + 'img/social-card.jpg',
+    navbar: {
+      title: 'O Código Dá 20',
+      hideOnScroll: true,
+      style: 'dark',
+      items: [
+        {
+          href: 'https://www.linkedin.com/in/marceloandreia/',
+          label: 'LinkedIn',
+          position: 'right',
+        },
+//         {
+//           type: 'localeDropdown',
+//           position: 'right',
+//         },
+      ],
+    },
+    footer: {
+      logo: {
+        alt: 'Logo OCDV',
+        src: contextPath + 'img/ocdv-logo.jpg',
+        height: 200,
+        width: 200,
+      },
+      style: 'dark',
+      links: [
+        {
+          html: ' ',
+        },
+        {
+          label: 'Arquivo',
+          to: 'arquivo',
+        },
+        {
+          label: 'Pagina Inicial',
+          to: '/',
+        },
+        {
+          label: 'Sobre Mim',
+          to: 'autores/marcelojr',
+        },
+        {
+          label: 'Tags',
+          to: 'tags',
+        },
+        {
+          label: 'Github',
+          href: 'https://www.github.com/marcelocell/',
+        },
+        {
+          label: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/marceloandreia/',
+        },
+        {
+          html: ' ',
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} - OCDV - O Código Dá 20. Construído com Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.dracula,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java'],
+    },
+    blog : {
+      sidebar: {
+        groupByYear: false,
+      },
+    },
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
+    metadata: [
+      {name: 'keywords', content: 'tech, technology, tecnologia, tec, desenvolvimento, programação, programming, developer, software, java'},
+    ],
+  } satisfies Preset.ThemeConfig,
+  titleDelimiter: "-",
+};
+
+export default config;
