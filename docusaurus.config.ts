71b632f4c3207318aa4db9206f51ceeed49b064d
Marcelo Junior <marcelo_andreia@hotmail.com>
2025-07-09 18:01:59 -0300

    build: Add build configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,6 +26,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'marcelocell', // Usually your GitHub org/user name.
   projectName: 'ocdv', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
